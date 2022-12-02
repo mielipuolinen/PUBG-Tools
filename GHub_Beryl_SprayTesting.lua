7449d84c708255d54bb7364dcac8dbe129b4af21
Niko Mielikäinen <4509558+mielipuolinen@users.noreply.github.com>
2022-12-02 15:50:55 +0200

    Update GHub_Beryl_SprayTesting.lua

--- GHub_Beryl_SprayTesting.lua
+++ GHub_Beryl_SprayTesting.lua
@@ -5,18 +5,21 @@
 -- Beryl, Full Auto, standing and breathing
 -- Red Dot, Compensator, Vertical Foregrip, Quickdraw Mag
 
+mouseBind_activation = 4 -- 4: thumb near, 5: thumb far
+mouseBind_fireInGame = 1 -- 1: LMB, 2: RMB
+
 function SetDefaults()
     ClearLog()
     magSize = 30 -- shots to fire
-    timePerShot = 86 -- firerate (ms)
+    timePerShot = 86 -- rate of fire (ms)
     mouseTimer_offset = math.floor( timePerShot / 2 )
-    moveX = 95 -- mouse X axis compensation per shot (px)
-    moveX_increasePerShot = 2 -- mouse X axis compensation increase per shot (px)
+    moveY = 95 -- mouse X axis compensation per shot (px)
+    moveY_increasePerShot = 2 -- mouse X axis compensation increase per shot (px)
     fireMode = "auto" -- auto/single/burst
 end
 
 function OnEvent(event, arg)
-    if (event == "MOUSE_BUTTON_PRESSED" and arg == 4) then
+    if (event == "MOUSE_BUTTON_PRESSED" and arg == MouseBind) then
         SetDefaults()
 
         if fireMode == "auto" then
@@ -34,12 +37,11 @@ function AutoFire()
     OutputLogMessage("AutoFire():\n")
     OutputLogMessage("magSize: " .. magSize .. " pcs, timePerShot: " .. timePerShot)
     OutputLogMessage(" ms, mouseTimer_offset: " .. mouseTimer_offset)
-    OutputLogMessage(" ms, moveX: " .. moveX .. " px, moveX_increasePerShot: " .. moveX_increasePerShot)
+    OutputLogMessage(" ms, moveY: " .. moveY .. " px, moveY_increasePerShot: " .. moveY_increasePerShot)
     OutputLogMessage(" px, fireMode: " .. fireMode .. "\n\n")
 
+    PressMouseButton(mouseBind_fireInGame)
     sprayTimer_start = GetRunningTime()
-
-    PressMouseButton(1)
     shotCounter = 1
 
     repeat
@@ -52,20 +54,21 @@ function AutoFire()
             actualTimer_f = math.floor(shotCounter * 85.71 - 85.71)
             actualTimer_c = math.ceil(shotCounter * 85.71 - 85.71)
             OutputLogMessage(shotCounter .. "/" .. magSize)
-            OutputLogMessage("\t moveX: " .. moveX .. " px\t\t mouseTimer: " .. mouseTimer .. " ms")
+            OutputLogMessage("\t moveY: " .. moveY .. " px\t\t mouseTimer: " .. mouseTimer .. " ms")
             OutputLogMessage("\t\t actual shot: " .. actualTimer_f .. " - " .. actualTimer_c .. " ms\n")
             OutputLogMessage("\t sprayTimer: " .. sprayTimer .. " ms\t sprayTimer_start: " .. sprayTimer_start .. " ms\n")
 
-            MoveMouseRelative(0,moveX)
+            MoveMouseRelative(0,moveY)
 
-            moveX = moveX + moveX_increasePerShot
+            moveY = moveY + moveY_increasePerShot
             shotCounter = shotCounter + 1
 
         end
 
     until shotCounter == magSize + 1 -- +1 includes last shot
 
-    ReleaseMouseButton(1)
+    ReleaseMouseButton(mouseBind_fireInGame)
+
 end
 
 function SingleFire()
